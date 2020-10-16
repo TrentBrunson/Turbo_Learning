adc6d5a92024e86efaeaa8f77dd5cc019aceb5d6
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-10-16 10:11:53 -0500

    Texas plays only

--- clean.py
+++ clean.py
@@ -10,6 +10,65 @@ from config import db_password
 
 # %%
 # read over data folder and open each csv
+
+# set dir location
+year = 2013  # set range of years to look at for loop
+dirYear = 'data/2013'
+
+# make a location to store only tex data by year
+path = "data/tex"
+
+try:
+    os.mkdir(path)
+except OSError:
+    print ("Creation of the directory %s failed; it already exists." % path)
+else:
+    print ("Successfully created the directory %s" % path)
+
+#%%
+# make this into f(x) and call in loop iterating through year
+year_data = pd.DataFrame()
+for f in glob.glob("data/2013/*.csv"):
+    df = pd.read_csv(f)
+    year_data = year_data.append(df,ignore_index=True)
+year_data.shape
+
+# get right columns for each year
+year_data = year_data[['gameId', 'year', 'week', 'homeAbbr', 'awayAbbr', 'offenseAbbr', 'defenseAbbr', 'homeScore', 'awayScore', 'quarter', 'clock', 'type', 'down', 'distance', 'yardLine', 'yardsGained']]
+
+# get only Texas data for both home and away games
+Tex_data = year_data[(year_data.homeAbbr == 'TEX') | (year_data.awayAbbr == 'TEX')]
+
+# write to csv
+Tex_data.to_csv('data/tex/Tex_data2013', sep= ',', index= False)
+Tex_data
+# %%
+# check out values in columns
+column_list = list(Tex_data.columns.tolist())
+Tex_data[column_list].nunique() 
+# %%
+# see list of values in play type column
+sorted(Tex_data.type.unique().tolist())
+# %%
+# get pass and rush plays only
+
+# %%
+
+# %%
+
+# %%
+Tex_data.shape
+# %%
+
+#%%
+year_data.shape
+# %%
+list(year_data)
+
+# iterate through loop for year
+
+# %%
+# read over data folder and open each csv
 # merge into single csv for data cleaning
 csv_destdir = ('/data/')
 AllData = "data/AllData.csv"
@@ -26,30 +85,7 @@ with open(AllData, 'w') as outfile:
     outfile.write('\n'.join(data))
 # adds file name to last column 2014+
 # keeps reading header
-#%%
-# drop columns to make file size more manageable
-inputFile = AllData
-outputFile = 'data/selectData.csv'
-# colsToRemove = [['homeId'], ['homeTeam'], ['awayId'], ['awayTeam'], ['driveIndex'], 
-#             ['playIndex'], ['offenseId'], ['offenseTeam'], ['defenseId'], ['defenseTeam'], 
-#             ['defenseAbbr'], ['wallclock'], ['endYardLine'], ['description']]
 
-colsToRemove = [0,3,4,6,7,9,10,11,12,14,15,16,21,27,28]
-# reverse rows to remove end first otherwise indexes keep shifting left
-colsToRemove = sorted(colsToRemove, reverse=True)
-rowCount = 0
-
-with open(inputFile, 'r') as source:
-    reader = csv.reader(source)
-    with open(outputFile, 'w', newline='') as result:
-        writer = csv.writer(result)
-        for row in reader:
-            rowCount += 1
-            # check progress thru file
-            print('\r{0}'.format(rowCount), end='')
-            for colIndex in colsToRemove:
-                del row[colIndex]
-            writer.writerow(row)
 #%%
 # merge data by year
 path = 'data/2013'
@@ -58,63 +94,24 @@ dfs = glob.glob('path/*.csv')
 result = pd.concat([pd.read_csv(df) for df in dfs], ignore_index=True)
 
 result.to_csv('path/merge.csv', ignore_index=True)
-#%%
-# make into DF
-df_CFB = pd.read_csv(AllData, sep=",", error_bad_lines=False)
-df_CFB
-#%%
-df_CFB.shape
+
 # %%
 # not all years are the same
 # iterating the columns to get column names
-list(df.columns)
+
 # read each year separately & clean columns
 # then merge complete years together
 # %%
 # evaluate data
 print(
-    f'CFB DB data types: {df_CFB.dtypes}\n\n'
-    f'Nulls: {df_CFB.isnull().sum}\n\n'
-    f'Not Nulls: {df_CFB.notnull().sum()}\n\n'
+    f'Tex DB data types: {Tex_data.dtypes}\n\n'
+    f'Nulls: {Tex_data.isnull().sum}\n\n'
+    f'Not Nulls: {Tex_data.notnull().sum()}\n\n'
 )
 
-for column in df_CFB.columns:
-    print(f'Column {column} has {df_CFB[column].isnull().sum()} null values.')
+for column in Tex_data.columns:
+    print(f'Column {column} has {Tex_data[column].isnull().sum()} null values.')
 
-print(f'Duplicate entries: {df_CFB.duplicated().sum()}')
+print(f'Duplicate entries: {Tex_data.duplicated().sum()}')
 
-#%%
-# Generate categorical variable list
-colList = df_CFB.index.tolist()
-
-# column_drop_list
-#%%
-# check number of unique values in each column
-df_CFB[df_CFB].nunique()
-#%%
-# drop of columns
-df_CFB_small = df_CFB.drop(df_CFB.columns['homeId', 'homeTeam', 'awayId',
-'awayTeam', 'driveIndex', 'playIndex', 'offenseId', 'offenseTeam',
-'defenseId', 'defenseTeam', 'defenseAbbr', 'wallclock', 'endYardLine',
-'description'], axis = 1) 
-  
-df_CFB_small
-#%%
-# create 2 df: 1 with all TEX defense, 2nd with all TEX off (2nd not needed)
-
-# remove columns before importing csv
-with open(AllData,"r") as source:
-    rdr= csv.reader(source)
-    with open("result","w") as result:
-        dest= csv.writer(result)
-        colDel= ((
-            ['homeId'], ['homeTeam'], ['awayId'], ['awayTeam'], ['driveIndex'], 
-            ['playIndex'], ['offenseId'], ['offenseTeam'], ['defenseId'], ['defenseTeam'], 
-            ['defenseAbbr'], ['wallclock'], ['endYardLine'], ['description']
-            ) for r in rdr )
-        dest.writerows(colDel)
 # %%
-print(dest)
-dest
-# %%
-#  get list of play types, keep plays, drop rest
