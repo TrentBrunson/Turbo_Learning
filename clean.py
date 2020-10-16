9a4e0bb34b5f78714171f5f79b78c82ae43a3a9e
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-10-15 22:25:46 -0500

    combined 2013-18 files, 951,297 records

--- clean.py
+++ clean.py
@@ -14,17 +14,18 @@ from config import db_password
 # merge into single csv for data cleaning
 csv_destdir = ('/data/')
 AllData = "data/AllData.csv"
-
-allFiles = glob.glob('**/*.csv', recursive= True)
-with open(AllData, 'wb') as outfile:
-    for i, fname in enumerate(allFiles):
-        with open(fname, 'rb') as infile:
-            shutil.copyfileobj(infile, outfile)
-
-
-# ---------------------------------------
 allFiles = glob.glob("**/*.csv", recursive = True)
 data = []
+# --------------------------------------------------
+
+# allFiles = glob.glob('**/*.csv', recursive= True)
+# with open(AllData, 'wb') as outfile:
+#     for i, fname in enumerate(allFiles):
+#         with open(fname, 'rb') as infile:
+#             shutil.copyfileobj(infile, outfile)
+
+# ---------------------------------------
+
 
 for i, fname in enumerate(allFiles):
     with open(fname, 'r') as infile:
@@ -32,7 +33,7 @@ for i, fname in enumerate(allFiles):
         read_lines = ['{0},{1}'.format(line.rstrip('\n'),fname) for line in read_lines]
         data += read_lines
 
-with open('D:\\AllData.csv', 'w') as outfile:
+with open(AllData, 'w') as outfile:
     outfile.write('\n'.join(data))
 #%%
 # drop columns
