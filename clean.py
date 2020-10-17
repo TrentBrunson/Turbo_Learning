55c54e804461d1d826163d614fd93ec67f3ebc16
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-10-16 19:06:30 -0500

    data processing code finalized; ready for pull

--- clean.py
+++ clean.py
@@ -7,14 +7,14 @@ import numpy as np
 import glob
 from sqlalchemy import create_engine
 from config import db_password
-
 # %%
-# read over data folder and open each csv
+# set end and start values at the beginning
+# no need to make this a user input
 start_year = 2013
 end_year = 2018
 
-# make a location to store only tex data by year
-path = "data/tex"
+# set up data folder to temp hold intermediate data processing step
+path = "data/tex"  # make a location to store only tex data by year
 
 try:
     os.mkdir(path)
@@ -22,19 +22,20 @@ except OSError:
     print ("Creation of the directory %s failed; it already exists." % path)
 else:
     print ("Successfully created the directory %s" % path)
-
 # %%
 def evaluate_data(df):
-    # evaluate data
+    # evaluate data by year; check data types and find nulls if any
     print(
         f'Tex DB data types: {df.dtypes}\n\n'
         f'Nulls: {df.isnull().sum}\n\n'
         f'Not Nulls: {df.notnull().sum()}\n\n'
     )
 
+    # after checking nulls in the dataframe, is any appear, check it by column
     for column in df.columns:
         print(f'Column {column} has {df[column].isnull().sum()} null values.')
 
+    # check for duplicate entries and see the dataframe shape
     print(
         f'Duplicate entries: {df.duplicated().sum()}'
         f'Dataframe shape: {df.shape}'
@@ -42,6 +43,7 @@ def evaluate_data(df):
     return
 # %%
 # make this into f(x) and call in loop iterating through year
+# f(x) takes in year value
 def combine_years(year): 
     path_string = f"data/{year}/*.csv"
     year_data = pd.DataFrame()
@@ -49,19 +51,18 @@ def combine_years(year):
         df = pd.read_csv(f, error_bad_lines=False)
         year_data = year_data.append(df,ignore_index=True)
 
-    # get right columns for each year
-    year_data = year_data[['gameId', 'year', 'week', 'homeAbbr', 'awayAbbr', 'offenseAbbr', 'defenseAbbr', 'homeScore', 'awayScore', 'quarter', 'clock', 'type', 'down', 'distance', 'yardLine', 'yardsGained']]
+    # get right columns for each year - dropping doesn't work because 2014 has issues
+    # specfically found week 6 to be problematic; dropped one line of data & it works
+    year_data = year_data[['gameId', 'year', 'week', 'homeAbbr', 'awayAbbr', 
+    'offenseAbbr', 'defenseAbbr', 'homeScore', 'awayScore', 'quarter', 'clock', 
+    'type', 'down', 'distance', 'yardLine', 'yardsGained']]
 
     # get only Texas data for both home and away games
     Tex_data = year_data[(year_data.homeAbbr == 'TEX') | (year_data.awayAbbr == 'TEX')]
     
-    # get pass and rush plays only
-    # no regex, runs faster
+    # get pass and rush plays only; no regex, so it runs faster
     df_Tex_plays = Tex_data[(Tex_data.type.str.contains('Pass', regex=False)) | (
         Tex_data.type.str.contains('Rush', regex=False))]
-
-    # write to csv
-    # df_Tex_plays.to_csv('data/tex/Tex_data2013', sep= ',', index= False)
     
     csv_path = f"data/tex/Tex_data{year}.csv"
     df_Tex_plays.to_csv(csv_path, sep= ',', index= False)
@@ -72,24 +73,26 @@ def combine_years(year):
         f'            ***** For year {year} *****   '
         f'\n---------------------------------------------------\n'
     )
+
+    # make sure that when pulling in only Texas data, it was fine
     evaluate_data(Tex_data)
 
+    # verfying that filtering down to only offensive plays worked
     evaluate_data(df_Tex_plays)
 
     return df_Tex_plays
-
 # %%
-# create for loop to iterate through rows
+# create for loop to iterate through the different folders organized by year
 for i in range (start_year, end_year+1):
     combine_years(i)
-
 # %%
-# combine tex folder files into one and save in main directory
-df_combined = pd.DataFrame()
+# combine tex folder files into one and save in main directory 
+# that will be published to github
+df_combined = pd.DataFrame()  # initialize dataframe
 for f in glob.glob("data/tex/*.csv"):
     df = pd.read_csv(f)
     df_combined = df_combined.append(df,ignore_index=True)
 
 df_combined.to_csv('Texas_combined.csv', index=False)
-df_combined.shape
+df_combined.shape # check that dataframe shape makes sense after years are combined
 # %%
