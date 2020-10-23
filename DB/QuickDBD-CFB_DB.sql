c243b13e266992d5a02c196ba96a8fc7bcdf1062
pablom15 <pablo.maldo15@gmail.com>
2020-10-23 17:29:09 -0500

    Updated ERD with new scores table

--- DB/QuickDBD-CFB_DB.sql
+++ DB/QuickDBD-CFB_DB.sql
@@ -1,29 +1,37 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE CFB_DB (
+CREATE TABLE "TEX_PBP" (
+    "playId" varchar   NOT NULL,
     "gameId" int   NOT NULL,
     "year" int   NOT NULL,
     "week" int   NOT NULL,
-    "homeAbbr" varchar   NOT NULL,
-    "awayAbbr" varchar   NOT NULL,
     "offenseAbbr" varchar   NOT NULL,
     "defenseAbbr" varchar   NOT NULL,
-    "homeScore" int   NOT NULL,
-    "awayScore" int   NOT NULL,
     "quarter" int   NOT NULL,
     "clock" time   NOT NULL,
     "type" varchar   NOT NULL,
     "down" int   NOT NULL,
     "distance" int   NOT NULL,
     "yardLine" int   NOT NULL,
-    "yardsGained" int   NOT NULL
+    "yardsGained" int   NOT NULL,
+    CONSTRAINT "pk_TEX_PBP" PRIMARY KEY (
+        "playId"
+     )
 );
 
-rollback;
-COMMIT;
+CREATE TABLE "TEX_Opp_Score" (
+    "playId" varchar   NOT NULL,
+    "homeAbbr" varchar   NOT NULL,
+    "awayAbbr" varchar   NOT NULL,
+    "homeScore" int   NOT NULL,
+    "awayScore" int   NOT NULL,
+    CONSTRAINT "pk_TEX_Opp_Score" PRIMARY KEY (
+        "playId"
+     )
+);
 
-SELECT * FROM CFB_DB;
+ALTER TABLE "TEX_Opp_Score" ADD CONSTRAINT "fk_TEX_Opp_Score_playId" FOREIGN KEY("playId")
+REFERENCES "TEX_PBP" ("playId");
 
-DELETE FROM CFB_DB;
