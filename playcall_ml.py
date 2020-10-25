c5a67d1920c0b69115ff55ff41a7f568dfd2c243
Travgreenfield <travgreenfield@gmail.com>
2020-10-25 14:41:21 -0500

    preprocessing time fix pre-binning

--- playcall_ml.py
+++ playcall_ml.py
@@ -6,10 +6,33 @@ from config import db_password
 import matplotlib as plt
 import sklearn as skl
 import tensorflow as tf
+from scipy import stats
 
 #%%
 # Import clean dataset from raw file
-data_df = pd.read_csv('Texas_combined.csv')
+data_df = pd.read_csv('Resources/Texas_combined.csv')
+
+#%%
+def time_convert(x):
+    m,s = map(int,x.split(':'))
+    return (m*60)+s
+
+data_df['seconds_in_quarter_remaining'] = data_df.clock.apply(time_convert)
+
+#%%
+data_df['seconds_in_half_remaining'] = data_df['seconds_in_quarter_remaining']
+data_df.loc[data_df.quarter== 1,'seconds_in_half_remaining':]*=2
+data_df.loc[data_df.quarter== 3,'seconds_in_half_remaining':]*=2
+
+#%%
+data_df.loc[data_df.quarter == 1, 'half'] = 1
+data_df.loc[data_df.quarter == 2, 'half'] = 1
+data_df.loc[data_df.quarter == 3, 'half'] = 2
+data_df.loc[data_df.quarter == 4, 'half'] = 2
+
+#%%
+# Preprocessing for ml model
+# Convert all pass outcomes to "pass"
 
 #%%
 # Initialize framework to import clean df from postgres db
@@ -36,6 +59,7 @@ features_df = data_df.drop(columns='type')
 
 #%%
 #Split into testing and training groups
+# Try and 50/50 and then 50/50 the testing data again to then apply to the final validationb. Don't let model see validation until the very end.
 
 #%%
 #Scale data based on testing group and apply to testing and training
