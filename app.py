f05e4fcb01f7c237893589cb3192313e82055df5
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-11-07 15:26:47 -0600

    reverting to original app.py

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ import pickle
 import numpy as np
 from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+def time_convert(x):
+    h,m,s = map(int,x.split(':'))
+    return (h*360)+(m*60)+s
 
+app = Flask(__name__)
 # start home page
-@app.route('/')
+@app.route("/")
 def index():
     return render_template('index.html')
 
@@ -15,51 +18,37 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     # get user inputs
+    distance = request.form['distance']
+    down = request.form['down']
     quarter = request.form['quarter']
     clock = request.form['clock']
-    down = request.form['down']
-    distance = request.form['distance']
 
-    # convert strings to numbers
-    quarter = int(quarter)
-    clock = int(clock)
-    down = int(down)
-    distance = int(distance)
-
-    # change quarters to halves
-    if quarter <= 2:
-        half = 1
-    elif quarter == 5:
-        half = 3
-    else:
-        half = 2
-
-    # convert clock to seconds so the ML model can take it in
-    if half == 1:
-        clockSeconds = clock * 60
-    elif half == 2:
-        clockSeconds = clock * 60 * 2
-    else:
-        clockSeconds = clock * 60 * 3
+    # convert clock to something the ML model can take in
+    # how is ML model using time???
+    # bin it - pick midpoint of each bin for value?
+    """***---???---***"""
+    """***---???---***"""
+    """***---???---***"""
 
     # take inputs and put into array, ready for ML model
-    feature_list = [half, clockSeconds, down, distance]
+    feature_list = [down, distance, quarter, clock]
     features = [np.array(feature_list)]
 
     # call the play
 
-    # load model from saved file
-    model = pickle.load(open('ML_models/random_forest/rfPickle.pkl', 'rb'))
+    # use pickle or H5?????
+    # load H5 from sklearn/pandas pd.read_hdf
+    model = pickle.load(open('???model.pkl'))
     prediction = model.predict(features)
     output = prediction[0]
 
     # binary output for pass or rush call
-    if output == 1:
+    if output == 0:
         result = 'Pass'
     else:
         result = 'Rush'
 
-    return render_template('index.html', call = f'{result} defense!')
+    return render_template('index.htnml', call = result)
 
 @app.route('/findings')
 def findings():
@@ -71,5 +60,3 @@ def methodology():
 
 if __name__ == "__main__":
     app.run()
-
-import pdb; pdb.set_trace()
